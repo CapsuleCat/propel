5f223b072ff55c4ca45d69fb34a0a985ee656536
Ivan Montiel <idmontie@gmail.com>
2022-10-03 11:02:15 -0700

    Bootstrap express AFTER other bootstraps

--- packages/propel-server/src/bootstrap.ts
+++ packages/propel-server/src/bootstrap.ts
@@ -12,11 +12,11 @@ export async function bootstrap() {
     const appBottle = getAppBottle();
 
     if (process.env.DRY_RUN !== "true") {
-        const expressDefer = appBottle.container._ExpressDefer as ExpressDefer;
-        expressDefer.execute();
-
         const serviceDefer = appBottle.container._ServiceDefer as ServiceDefer;
         await serviceDefer.execute();
+
+        const expressDefer = appBottle.container._ExpressDefer as ExpressDefer;
+        expressDefer.execute();
     }
 
     return appBottle.container;
