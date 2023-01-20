e27215f2cc7e46cc661ad47c73e3fdbfafb4e2e5
Ivan Montiel <idmontie@gmail.com>
2023-01-20 15:02:54 -0700

    feat: add propel cli

---
+++ packages/propel-cli/README.md
@@ -0,0 +1,11 @@
+# propel-cli
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build propel-cli` to build the library.
+
+## Running unit tests
+
+Run `nx test propel-cli` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/propel-cli/src/index.ts
@@ -0,0 +1,17 @@
+import { Command } from "commander";
+import { graph } from "./lib/commands/graph";
+
+const program = new Command();
+
+program.name("propel-cli").description("CLI for Propel");
+
+program
+    .command("graph")
+    .description(
+        "Generate a mermaid graph of the services and their dependencies"
+    )
+    .argument("<cmd>", "Command to run")
+    .argument("<ouput>", "Output file, example graph.svg")
+    .action(graph);
+
+program.parse(process.argv);
