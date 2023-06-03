fb220c76c081e53d5e1710da189c0a11d2ba314f
Ivan Montiel <idmontie@gmail.com>
2023-06-03 12:11:13 -0700

    fix: fix test bootstrapping

--- packages/propel-express/src/lib/PropelExpressPlugin.ts
+++ packages/propel-express/src/lib/PropelExpressPlugin.ts
@@ -49,8 +49,8 @@ export class PropelExpressPlugin implements PropelPlugin {
             app?.use(bodyParser.json());
         }
 
-        executeControllerStack();
-
         await executeBootstrapStack();
+
+        executeControllerStack();
     }
 }
